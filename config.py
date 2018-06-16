7f8b883772750a9710aa10d7e8cbeff3cc110fe6
Paul E Carpenter <paulecar@mac.com>
2018-06-16 16:10:39 -0400

    Changed to use YRD user ID and tidy up team9.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     # MySql DB configurations
     # TODO Create application account and stop using root/pwd combo
     SQLALCHEMY_DATABASE_URI = \
-        os.environ.get('DATABASE_URL') or 'mysql://root:Cerbera1#@localhost:3306/AmsterdamTeam9'
+        os.environ.get('DATABASE_URL') or 'mysql://yrd:amsterdam@localhost:3306/AmsterdamTeam9'
     # SqlLite format - 'localhost:3306' + os.path.join(basedir, 'team9.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Email Support
